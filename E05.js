26c9370a35d44f0e5fe80c29206d86cb23208ee0
Gabriel Machado Violante <101432741+ViolanteGabriel@users.noreply.github.com>
2022-06-09 19:05:18 -0300

    Tabuleiro de Xadrez

--- E05.js
+++ E05.js
@@ -2,7 +2,7 @@
 // Date: 07/06/2022
 
 // Function 1: Build a function in JavaScript that prints a triangle in the console
-drawingTriangle(10);
+//drawingTriangle(10);
 function drawingTriangle(nLines){
     for (let i = 0; i < nLines; i++){
 
@@ -11,6 +11,23 @@ function drawingTriangle(nLines){
             lineContent += "#";
         }
         
+        console.log(lineContent);
+    }
+}
+
+// Function 2: Build a function in JavaScript that prints a chessboard in the console
+//chessBoard(10);
+function chessBoard(nLines){
+    for (let i = 0; i < nLines; i++){
+        if (i % 2 == 0) { lineContent = "|O|"; }
+        if (i % 2 != 0) { lineContent = "|X|"; }
+
+        for (let j = 0; j < nLines-1; j++){
+            if (i % 2 == 0) { j++; }
+            if (j % 2 == 0) { lineContent += "|O|"; }
+            if (j % 2 != 0) { lineContent += "|X|"; }
+            if (i % 2 == 0) { j--; }
+        }
         console.log(lineContent);
     }
 }
